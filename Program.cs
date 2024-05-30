9da5666c9e5f74f86b722dbd9248ae08addb9014
Mari-Anne Mee <mari-anne@MariAnnesLaptop.lan>
2024-05-30 14:29:08 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+
+
+
